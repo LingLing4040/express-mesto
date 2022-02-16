d114787ef6a1744cd1faa50d7edfa4bd16c8ec64
Nikita <filatovnikita1994@yandex.ru>
2022-02-16 15:57:09 +0300

    Обновить

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Требуется авторизация');
+    throw new UnauthorizedError('Требуется авторизация!');
   }
 
   const token = extractBearerToken(authorization);
@@ -19,7 +19,7 @@ module.exports = (req, res, next) => {
   } catch (err) {
     if (err.name === 'JsonWebTokenError') {
       next(
-        new UnauthorizedError('Требуется авторизация!'),
+        new UnauthorizedError('Требуется авторизация'),
       );
     } else {
       next(err);
