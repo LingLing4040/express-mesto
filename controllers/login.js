7fe0e27aeba5f5109a5e65fc7ba71ff1276e3f14
Nikita <filatovnikita1994@yandex.ru>
2022-02-16 15:35:09 +0300

    Обновить

--- controllers/login.js
+++ controllers/login.js
@@ -10,11 +10,10 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
 
-      res.send({ token });
-      // res.cookie('jwt', token, {
-      //   maxAge: 3600000,
-      //   httpOnly: true,
-      // });
+      res.cookie('jwt', token, {
+        maxAge: 3600000,
+        httpOnly: true,
+      });
     })
     .catch((err) => {
       if (err.code === codes.UNAUTHORIZED_CODE) {
