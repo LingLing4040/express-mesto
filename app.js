50e0648b8b9e9d58e156275e121448e7018f4d39
Nikita <filatovnikita1994@yandex.ru>
2022-02-17 13:10:11 +0300

    Откатить

--- app.js
+++ app.js
@@ -1,11 +1,72 @@
-require('dotenv').config();
+// require('dotenv').config();
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
+// const { celebrate, Joi, errors } = require('celebrate');
+// const validator = require('validator');
+// const cors = require('cors');
+// const errorHandler = require('./middlewares/error-handler');
+// const router = require('./routes');
+// const { createUser } = require('./controllers/users');
+// const { login } = require('./controllers/login');
+// const auth = require('./middlewares/auth');
+
+// const method = (value) => {
+//   const result = validator.isURL(value);
+//   if (result) {
+//     return value;
+//   }
+//   throw new Error('URL validation err');
+// };
+
+// const { PORT = 3000 } = process.env;
+
+// const app = express();
+
+// app.use(cors({
+//   origin: 'http://cool.domainname.students.nomoredomains.xyz',
+//   // origin: 'http://localhost:3000',
+//   credentials: true,
+// }));
+
+// app.use(bodyParser.json());
+// app.use(cookieParser());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.post('/signin', celebrate({
+//   body: Joi.object().keys({
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required().min(8),
+//   }),
+// }), login);
+// app.post('/signup', celebrate({
+//   body: Joi.object().keys({
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required().min(8),
+//     avatar: Joi.string().custom(method),
+//   }),
+// }), createUser);
+// app.use(auth);
+// app.use(router);
+// app.use(errors());
+// app.use(errorHandler);
+
+// mongoose.connect('mongodb://localhost:27017/mestodb', {
+//   useNewUrlParser: true,
+//   // useCreateIndex: true,
+//   //   useFindAndModify: false
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}`);
+// });
+
+const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const { celebrate, Joi, errors } = require('celebrate');
 const validator = require('validator');
-const cors = require('cors');
 const errorHandler = require('./middlewares/error-handler');
 const router = require('./routes');
 const { createUser } = require('./controllers/users');
@@ -24,14 +85,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://cool.domainname.students.nomoredomains.xyz',
-  // origin: 'http://localhost:3000',
-  credentials: true,
-}));
-
 app.use(bodyParser.json());
-app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -46,6 +100,10 @@ app.post('/signup', celebrate({
     avatar: Joi.string().custom(method),
   }),
 }), createUser);
+// app.use(cookieParser());
+// app.get('/posts', (req, res) => {
+//   console.log(req.cookies.jwt);
+// });
 app.use(auth);
 app.use(router);
 app.use(errors());
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,33 +1,63 @@
-require('dotenv').config();
+// require('dotenv').config();
+// const jwt = require('jsonwebtoken');
+// const UnauthorizedError = require('../errors/unauthorized-error');
+
+// const { NODE_ENV, JWT_SECRET } = process.env;
+
+// // eslint-disable-next-line consistent-return
+// module.exports = (req, res, next) => {
+//   const { token } = req.cookies.jwt;
+
+//   if (!token) {
+//     throw new UnauthorizedError('Требуется авторизация!');
+//   }
+
+//   let payload;
+
+//   try {
+//     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'jwtsecret');
+//   } catch (err) {
+//     if (err.name === 'JsonWebTokenError') {
+//       next(
+//         new UnauthorizedError('Требуется авторизация'),
+//       );
+//     } else {
+//       // next(err);
+//       throw new UnauthorizedError('Требуется авторизация!!');
+//     }
+//   }
+
+//   req.user = payload;
+
+//   next();
+// };
+
 const jwt = require('jsonwebtoken');
+
 const UnauthorizedError = require('../errors/unauthorized-error');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
-
+const extractBearerToken = (header) => header.replace('Bearer ', '');
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { token } = req.cookies.jwt;
+  const { authorization } = req.headers;
 
-  if (!token) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new UnauthorizedError('Требуется авторизация!');
   }
 
+  const token = extractBearerToken(authorization);
   let payload;
-
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'jwtsecret');
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
     if (err.name === 'JsonWebTokenError') {
       next(
         new UnauthorizedError('Требуется авторизация'),
       );
     } else {
-      // next(err);
-      throw new UnauthorizedError('Требуется авторизация!!');
+      next(err);
     }
   }
-
   req.user = payload;
-
   next();
 };
