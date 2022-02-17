20c0d3f1fd10fbdaa2408bd89991ca6c7c84374f
Nikita <filatovnikita1994@yandex.ru>
2022-02-17 14:46:47 +0300

    Обновить

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,17 +2,17 @@ const jwt = require('jsonwebtoken');
 
 const UnauthorizedError = require('../errors/unauthorized-error');
 
-const extractBearerToken = (header) => header.replace('Bearer ', '');
+// const extractBearerToken = (header) => header.replace('Bearer ', '');
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  console.log(req.headers);
+  // const { authorization } = req.headers;
+  const { token } = req.cookies.jwt;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  if (!token) {
     throw new UnauthorizedError('Требуется авторизация!');
   }
 
-  const token = extractBearerToken(authorization);
+  // const token = extractBearerToken(authorization);
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
