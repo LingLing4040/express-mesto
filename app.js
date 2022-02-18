c28bc6e97bf8b772b6ac20da839057eefe7b867b
Nikita <filatovnikita1994@yandex.ru>
2022-02-18 11:28:44 +0300

    CORS

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const { celebrate, Joi, errors } = require('celebrate');
 const validator = require('validator');
+const cors = require('cors');
 const errorHandler = require('./middlewares/error-handler');
 const router = require('./routes');
 const { createUser } = require('./controllers/users');
@@ -84,6 +85,12 @@ const method = (value) => {
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://filatov.students.nomoredomains.work',
+  // origin: 'http://localhost:3000',
+  credentials: true,
+}));
 
 app.use(cookieParser());
 app.use(bodyParser.json());
