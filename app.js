8af89998dac799b0c80ed504fb697d6a4cd2173c
Nikita <filatovnikita1994@yandex.ru>
2022-02-18 09:51:04 +0300

    Убрать авторизацию

--- app.js
+++ app.js
@@ -62,48 +62,48 @@
 // });
 
 const express = require('express');
-
-const { PORT = 3000 } = process.env;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-// const { celebrate, Joi, errors } = require('celebrate');
-// const validator = require('validator');
+const { celebrate, Joi, errors } = require('celebrate');
+const validator = require('validator');
 const errorHandler = require('./middlewares/error-handler');
 const router = require('./routes');
-// const { createUser } = require('./controllers/users');
-// const { login } = require('./controllers/users');
+const { createUser } = require('./controllers/users');
+const { login } = require('./controllers/users');
 // const auth = require('./middlewares/auth');
 
-// const method = (value) => {
-//   const result = validator.isURL(value);
-//   if (result) {
-//     return value;
-//   }
-//   throw new Error('URL validation err');
-// };
+const method = (value) => {
+  const result = validator.isURL(value);
+  if (result) {
+    return value;
+  }
+  throw new Error('URL validation err');
+};
+
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.post('/signup', celebrate({
-//   body: Joi.object().keys({
-//     email: Joi.string().required().email(),
-//     password: Joi.string().required().min(8),
-//     avatar: Joi.string().custom(method),
-//   }),
-// }), createUser);
-// app.post('/signin', celebrate({
-//   body: Joi.object().keys({
-//     email: Joi.string().required().email(),
-//     password: Joi.string().required().min(8),
-//   }),
-// }), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    avatar: Joi.string().custom(method),
+  }),
+}), createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
 // app.use(auth);
 app.use(router);
-// app.use(errors());
+app.use(errors());
 app.use(errorHandler);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,35 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
-const validator = require('validator');
+// const { celebrate, Joi } = require('celebrate');
+// const validator = require('validator');
 const NotFoundError = require('../errors/not-found-error');
 
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 // const auth = require('../middlewares/auth');
-const { createUser } = require('../controllers/users');
-const { login } = require('../controllers/users');
+// const { createUser } = require('../controllers/users');
+// const { login } = require('../controllers/users');
 
-const method = (value) => {
-  const result = validator.isURL(value);
-  if (result) {
-    return value;
-  }
-  throw new Error('URL validation err');
-};
+// const method = (value) => {
+//   const result = validator.isURL(value);
+//   if (result) {
+//     return value;
+//   }
+//   throw new Error('URL validation err');
+// };
 
-router.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-    avatar: Joi.string().custom(method),
-  }),
-}), createUser);
-router.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), login);
+// router.post('/signup', celebrate({
+//   body: Joi.object().keys({
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required().min(8),
+//     avatar: Joi.string().custom(method),
+//   }),
+// }), createUser);
+// router.post('/signin', celebrate({
+//   body: Joi.object().keys({
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required().min(8),
+//   }),
+// }), login);
 
 router.use('/', usersRouter);
 router.use('/', cardsRouter);
