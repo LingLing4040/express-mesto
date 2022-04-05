738b8e3b53749526cec84ec9bdc67a80bdf6bc63
Nikita <filatovnikita1994@yandex.ru>
2022-04-05 15:40:48 +0300

    Поправить запрос на изменение аватара

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
-const validator = require('validator');
+const router = require("express").Router();
+const { celebrate, Joi } = require("celebrate");
+const validator = require("validator");
 
 const method = (value) => {
   const result = validator.isURL(value);
   if (result) {
     return value;
   }
-  throw new Error('URL validation err');
+  throw new Error("URL validation err");
 };
 
 const {
@@ -16,28 +16,37 @@ const {
   updateProfile,
   updateAvatar,
   getMe,
-} = require('../controllers/users');
+} = require("../controllers/users");
 
-router.get('/users', getUsers);
-router.get('/users/me', getMe);
-router.get('/users/:userId', celebrate({
-  params: Joi.object().keys({
-    userId: Joi.string().hex().length(24).required(),
+router.get("/users", getUsers);
+router.get("/users/me", getMe);
+router.get(
+  "/users/:userId",
+  celebrate({
+    params: Joi.object().keys({
+      userId: Joi.string().hex().length(24).required(),
+    }),
   }),
-}), getUser);
-router.patch('/users/me', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).default('Жак-Ив Кусто')
-      .required(),
-    about: Joi.string().min(2).max(30).default('Исследователь')
-      .required(),
+  getUser
+);
+router.patch(
+  "/users/me",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30).default("Жак-Ив Кусто").required(),
+      about: Joi.string().min(2).max(30).default("Исследователь").required(),
+    }),
   }),
-}), updateProfile);
-router.patch('/users/me/avatar', celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string().required().custom(method),
-    link: Joi.string().required().custom(method),
+  updateProfile
+);
+router.patch(
+  "/users/me/avatar",
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string().required().custom(method),
+    }),
   }),
-}), updateAvatar);
+  updateAvatar
+);
 
 module.exports = router;
